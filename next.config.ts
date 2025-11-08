ca8526226b9a2d30f74159f26869ec20c8cd4439
andymdavid <andy@otherstuff.studio>
2025-11-08 15:04:22 +0800

    Config: set basePath/assetPrefix for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/clone-site",
+  assetPrefix: "/clone-site",
 };
 
 export default nextConfig;
